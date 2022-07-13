b9d7a7499009f40c09b051fc09cd4e7e9d8f00f3
Akshay Serrao <akshay.serrao@ril.com>
2022-07-13 12:16:39 +0530

    not needed files removed

--- Machine Test/services/dbQuery.js
+++
@@ -1,8 +0,0 @@
-const dbQuery = async (qString, values) => {
-	const conn = await pool.getConnection()
-	const dbResult = await conn.query(qString, values)
-	conn.destroy()
-	return dbResult
-}
-
-module.exports = dbQuery
--- Machine Test/services/encryptDecrypt.js
+++
@@ -1,9 +0,0 @@
-const myDecrypt = (pass) => {
-	return pass ? Buffer.from(pass, 'base64').toString('ascii') : pass
-}
-
-const myEncrypt = (val) => {
-	return val ? Buffer.from(val, 'ascii').toString('base64') : val
-}
-
-module.exports = { myDecrypt, myEncrypt }
